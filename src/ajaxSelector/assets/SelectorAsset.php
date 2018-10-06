400208396b73e8ffd9cde6a5591f6906c3633824
Vitaliy <theyamshikov@yandex.ru>
2018-10-07 02:06:11 +0600

    clean

--- src/ajaxSelector/assets/SelectorAsset.php
+++ src/ajaxSelector/assets/SelectorAsset.php
@@ -2,6 +2,12 @@
 
 namespace yii2lab\widgets\ajaxSelector\assets;
 
+/**
+ * Class SelectorAsset
+ *
+ * @package yii2lab\widgets\ajaxSelector\assets
+ * @deprecated
+ */
 class SelectorAsset extends \yii2lab\extension\widget\ajaxSelector\assets\SelectorAsset
 {
 
